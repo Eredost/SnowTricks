28996674b141c4a1a5fd1fbb1facfbee498f2db2
Eredost <michael.coutin62@gmail.com>
2021-02-08 15:04:49 +0100

    feat: add basic validation constraints for the entities

--- src/Entity/Media.php
+++ src/Entity/Media.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\MediaRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=MediaRepository::class)
@@ -19,11 +20,25 @@ class Media
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(
+     *     message = "La source ne peut pas être vide"
+     * )
+     * @Assert\Length(
+     *     max = 255,
+     *     maxMessage = "La source ne peut pas dépasser {{ limit }} caractères"
+     * )
      */
     private $src;
 
     /**
      * @ORM\Column(type="string", length=60)
+     * @Assert\NotBlank(
+     *     message = "Le type de media ne peut pas être vide"
+     * )
+     * @Assert\Length(
+     *     max = 60,
+     *     maxMessage = "Le type de media ne peut pas dépasser {{ limit ]] caractères"
+     * )
      */
     private $type;
 
