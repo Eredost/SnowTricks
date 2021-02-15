ad9c8c09fbd4c4b3cc2bfe129bd91e2f3516d576
Eredost <michael.coutin62@gmail.com>
2021-02-15 11:53:50 +0100

    feat: integrate tricks section

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,9 +13,17 @@ class MainController extends AbstractController
      * @Route("/",
      *     name="homepage",
      *     methods={"GET"})
+     *
+     * @param TrickRepository $trickRepository
+     *
+     * @return Response
      */
-    public function index(): Response
+    public function index(TrickRepository $trickRepository): Response
     {
-        return $this->render('frontend/home.html.twig');
+        $tricks = $trickRepository->findBy([], null, 15);
+
+        return $this->render('frontend/home.html.twig', [
+            'tricks' => $tricks,
+        ]);
     }
 }
