9b479e128d6c7ccb84b7d7fe18ef64d395df3b74
Eredost <michael.coutin62@gmail.com>
2021-03-01 18:16:18 +0100

    feat: implementation of the videos section on the form for adding tricks and adding basic css styles

--- assets/trick_collection.js
+++ assets/trick_collection.js
@@ -1,8 +1,15 @@
 let trickCollection = {
     init: function () {
+        // Handle add new media buttons
         let addButtonElements = document.getElementsByClassName('new_trick_media');
         for (let addButtonElement of addButtonElements) {
             addButtonElement.addEventListener('click', trickCollection.handleAddButtonClick);
+        }
+
+        // Handle close buttons
+        let closeButtonElements = document.getElementsByClassName('js-delete-media');
+        for (let closeButtonElement of closeButtonElements) {
+            closeButtonElement.addEventListener('click', trickCollection.handleCloseButtonClick);
         }
     },
 
@@ -14,13 +21,25 @@ let trickCollection = {
     },
 
     addFormToCollection: function (prototype, wrapper) {
-        let newForm = document.createElement('div');
-        let count = wrapper.childElementCount;
+        let newForm = wrapper.querySelector('.js-media-template').cloneNode(true);
+        newForm.classList.remove('hide');
+        newForm.querySelector('.js-delete-media').addEventListener('click', trickCollection.handleCloseButtonClick);
+        let count = wrapper.childElementCount - 1;
 
         prototype = prototype.replace(/__name__/g, count);
-        newForm.innerHTML = prototype;
+
+        let prototypeWrapper = document.createElement('div');
+        prototypeWrapper.innerHTML = prototype;
+        newForm.prepend(prototypeWrapper.firstChild);
 
         wrapper.appendChild(newForm);
+    },
+
+    handleCloseButtonClick: function (e) {
+        e.preventDefault();
+
+        let parentElement = e.currentTarget.parentNode;
+        parentElement.remove();
     }
 };
 
