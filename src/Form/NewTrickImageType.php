933af384d364f8152be7ad1fddd4df8d2e9b6e3f
Eredost <michael.coutin62@gmail.com>
2021-03-01 13:35:08 +0100

    feat: handle images upload on addition trick form

--- src/Form/NewTrickImageType.php
+++ src/Form/NewTrickImageType.php
@@ -2,23 +2,20 @@
 
 namespace App\Form;
 
-use App\Entity\Media;
+use App\Entity\TrickImage;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Image;
 
-class NewImageType extends AbstractType
+class NewTrickImageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('src', FileType::class, [
+            ->add('file', FileType::class, [
                 'label'       => false,
-                'constraints' => [
-                    new Image()
-                ],
+                'required'    => false,
             ])
         ;
     }
@@ -26,7 +23,7 @@ class NewImageType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Media::class,
+            'data_class' => TrickImage::class,
         ]);
     }
 }
